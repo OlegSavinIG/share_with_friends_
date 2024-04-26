80330ea23c2cddd83c02e3ae29b4304507ded76c
OlegSavinIG <oleg211190gm@yandex.ru>
2024-04-26 10:07:56 +0300

    style changes

--- src/main/java/ru/practicum/shareit/base/BaseController.java
+++ src/main/java/ru/practicum/shareit/base/BaseController.java
@@ -9,48 +9,48 @@ import javax.validation.Valid;
 import java.util.List;
 
 
-public abstract class BaseController<T, ID> {
+public abstract class BaseController<T, D> {
 
     @PostMapping
     public ResponseEntity<T> create(@Valid @RequestBody T entity,
-                                    @RequestHeader(name = "X-Sharer-User-Id", required = false) ID userId) {
+                                    @RequestHeader(name = "X-Sharer-User-Id", required = false) D userId) {
         T createdEntity = createEntity(entity, userId);
         return ResponseEntity.ok(createdEntity);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<T> getById(@PathVariable ID id) {
+    public ResponseEntity<T> getById(@PathVariable D id) {
         T entity = getEntityById(id);
         return entity != null ? ResponseEntity.ok(entity) : ResponseEntity.notFound().build();
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<T> update(@Validated(Update.class) @RequestBody T entity,
-                                    @PathVariable ID id,
-                                    @RequestHeader(name = "X-Sharer-User-Id", required = false) ID userId) {
+                                    @PathVariable D id,
+                                    @RequestHeader(name = "X-Sharer-User-Id", required = false) D userId) {
         T updatedEntity = updateEntity(entity, id, userId);
         return updatedEntity != null ? ResponseEntity.ok(updatedEntity) : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable ID id) {
+    public ResponseEntity<Void> delete(@PathVariable D id) {
         boolean deleted = deleteEntity(id);
         return deleted ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<T>> getAll(@RequestHeader(name = "X-Sharer-User-Id", required = false) ID userId) {
+    public ResponseEntity<List<T>> getAll(@RequestHeader(name = "X-Sharer-User-Id", required = false) D userId) {
         List<T> entities = getAllEntities(userId);
         return ResponseEntity.ok(entities);
     }
 
-    protected abstract T createEntity(T entity, ID userId);
+    protected abstract T createEntity(T entity, D userId);
 
-    protected abstract T getEntityById(ID id);
+    protected abstract T getEntityById(D id);
 
-    protected abstract T updateEntity(T entity, ID id, ID userId);
+    protected abstract T updateEntity(T entity, D id, D userId);
 
-    protected abstract boolean deleteEntity(ID id);
+    protected abstract boolean deleteEntity(D id);
 
-    protected abstract List<T> getAllEntities(ID userId);
+    protected abstract List<T> getAllEntities(D userId);
 }

b0c515760da56d6d54a4e1bbb8c26c5f740115da
OlegSavinIG <oleg211190gm@yandex.ru>
2024-06-10 21:00:46 +0300

    tests 10

--- src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -20,7 +20,6 @@ import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
